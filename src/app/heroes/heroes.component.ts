a2a867835acf9406b6d8f7366810d1fc0f9f03d8
sundara mahalingam <catch.source@gmail.com>
2018-05-01 17:36:23 +0530

    Select Component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,15 +8,16 @@ import { HEROES } from '../mock-hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  hero : Hero ={
-  	id: 1,
-  	name: "Windstorm",
-  };
+  notes = "Welcome!!";
 
   heroes = HEROES;
 
-  notes = "Welcome!!";
+// declaring object of type Hero
+  selectedHero: Hero;
+
+  onSelect(hero: Hero ): void {
+  	this.selectedHero = hero;
+  }
 
   constructor() {}
 
