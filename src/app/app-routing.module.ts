efb68ab6083a80179d02e6c473bfb4060feb2699
sundara mahalingam <catch.source@gmail.com>
2018-05-31 16:29:02 +0530

    Simple switch between DashBoard & HeroList

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HeroesComponent } from './heroes/heroes.component';
 
+import { DashboardComponent }   from './dashboard/dashboard.component';
+
+
 const routes: Routes = [
-	{path: 'heroes', component: HeroesComponent }
+	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+	{ path: 'heroes', component: HeroesComponent },
+	{ path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
